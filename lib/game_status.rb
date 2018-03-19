0c7e95c9486e20c071f3a47737a9822fd1a44ef6
valeria campilongo <cvaleriac@hotmail.it>
2018-03-19 18:05:07 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -12,6 +12,6 @@ WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,4,8],[2,4,6],[0,3,6],[1,4,7],[2,5
   end
 end
 def full?(board)
-board.none? do |full_board| && board.none? do |incomplete_board|
+board.none? do |full_board| || board.none? do |incomplete_board|
   end
 end
