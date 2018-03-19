b28076e457d2326fac3a9e811b62c2b3ba5c70e4
valeria campilongo <cvaleriac@hotmail.it>
2018-03-19 17:14:12 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -6,7 +6,7 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,4,8],[2,4,6],[0,3,6],[1,4,7],[2,5,8]]
 
 def won?
-  WIN_COMBINATIONS do |WIN_COMBINATION|
-  puts WIN_COMBINATION
+  win_combination do |win_combination|
+  puts win_combination
   end
 end
