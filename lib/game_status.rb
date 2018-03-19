7cf07c2347fb2d11c853f6c4313bc34fd6f8b017
valeria campilongo <cvaleriac@hotmail.it>
2018-03-19 14:56:34 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -3,5 +3,9 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-WIN_COMBINATIONS = [[0,1,2], [3,4,5],[6,7,8], [0,4,8],[2,4,6], [0,3,6],[1,4,7],[2,5,8]]
-# Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,4,8],[2,4,6],[0,3,6],[1,4,7],[2,5,8]]
+
+def won?(board)
+  if (board[index].nil? || board[index] == " ")
+    return false
+  end
